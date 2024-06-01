aa781770e58a638daec78defb4f7160f27aff17a
Owoeye Samuel <owoeye1321@gmail.com>
2024-06-01 23:49:49 +0100

    xunit test practice with c#

---
+++ UnitTest/Test/WorldDumbestFunctionTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace UnitTest.Test
+{
+	public static class WorldDumbestFunctionTest
+	{
+		public static void WorldDumbestFunction_ReturnPikachuIfZero_ReturnString()
+		{
+			try {
+                //Arrange- Go get your variables, what you need, your classes
+                WorldDumbestFunction worldDumbestFunction = new WorldDumbestFunction();
+                int digit = 0;
+				//Act - Execute your classes/function
+				string result = worldDumbestFunction.ReturnPikachuIfZero(digit);
+				//Assert- Whatever is returned is what you want
+				if (result == "Pikachu")
+				{
+                     Console.WriteLine("Passed: Pikachu");
+				}
+				else
+				{
+					Console.WriteLine("Failed: Not Match");
+				}
+				
+			}catch(Exception ex)
+			{
+				Console.WriteLine($"What happened here >>>>>> {ex.Message}");
+			}
+		}
+	}
+}
+
---
+++ UnitTest/WorldDumbestFunction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace UnitTest
+{
+	public class WorldDumbestFunction
+	{
+		public string ReturnPikachuIfZero(int digit)
+		{
+			if (digit < 1) return "Pikachu";
+			return "Zero";
+		}
+}
+
