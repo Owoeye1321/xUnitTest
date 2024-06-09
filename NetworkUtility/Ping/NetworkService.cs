ac3bf9d8cc2330c8acb059ede58425fea2d8733e
Owoeye Samuel <owoeye1321@gmail.com>
2024-06-09 22:04:37 +0100

    holy fuck!

--- NetworkUtility/Ping/NetworkService.cs
+++ NetworkUtility/Ping/NetworkService.cs
@@ -26,6 +26,27 @@ namespace NetworkUtility.Ping
                 Ttl = 1
             };
         }
+        public IEnumerable<PingOptions> GetAllPingOptions()
+        {
+            IEnumerable<PingOptions> pingOptions = new[]
+            {
+                new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1
+            },
+                new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1
+            },new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1
+            },
+        };
+            return pingOptions;
+        }
     }  
 }
 
